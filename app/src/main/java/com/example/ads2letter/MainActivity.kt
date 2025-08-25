906225925231006baa34ec15ba959f7496eea112
laurinhuebner <133902335+laurinhuebner@users.noreply.github.com>
2025-08-25 11:58:26 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/ads2letter/MainActivity.kt
+++ app/src/main/java/com/example/ads2letter/MainActivity.kt
@@ -1,41 +1,19 @@
-package com.example.ads2letter
+package com.example.webviewapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var webView: WebView
-    private val startUrl = "http://192.168.1.21:5000" // deine URL
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        webView = findViewById(R.id.webview)
-        webView.webViewClient = WebViewClient()
+        val webView = WebView(this)
+        setContentView(webView)
 
         val settings: WebSettings = webView.settings
         settings.javaScriptEnabled = true
-        settings.domStorageEnabled = true
-        settings.allowFileAccess = true
-        settings.useWideViewPort = true
-        settings.loadWithOverviewMode = true
-        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-
-        webView.loadUrl(startUrl)
-    }
-
-    override fun onBackPressed() {
-        if (this::webView.isInitialized && webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        // Deine URL:
+        webView.loadUrl("http://192.168.1.21:5000")
     }
 }
